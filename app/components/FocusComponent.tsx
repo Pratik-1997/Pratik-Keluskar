4ab72d0fead9f7be39deb41cd586bd6de64378a8
Pratik-1997 <pratikkeluskar1997@gmail.com>
2024-01-16 18:13:44 +0530

    fixes

--- app/components/FocusComponent.tsx
+++ app/components/FocusComponent.tsx
@@ -10,7 +10,7 @@ const FocusComponent = () => {
     if (focus) {
       // Adjust the transparent percentage to make the radial background smaller
       const transparentPercentage = 24; // Change this value as needed
-      focus.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(190, 255, 255, 0.2), transparent ${transparentPercentage}%)`;
+      focus.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(29, 78, 216, 0.15), transparent ${transparentPercentage}%)`;
     }
   };
 
--- app/page.tsx
+++ app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
 
   return (
     <main>
-      <div className="focus"></div>
-      <FocusComponent />
+      {/* <div className="focus"></div>
+      <FocusComponent /> */}
       <div className="container">
         <div className="left-block block">
           <h1 className='name'>Pratik Keluskar</h1>
