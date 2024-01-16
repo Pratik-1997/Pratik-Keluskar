28b9f378a36693117bf1e851b29b6c4e3638e709
Pratik-1997 <pratikkeluskar1997@gmail.com>
2024-01-16 18:04:22 +0530

    FocusCursor

---
+++ app/components/FocusComponent.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+const FocusComponent = () => {
+  const handleMouseMove = (e: { pageX: any; pageY: any; }) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    const focus = document.querySelector('.focus');
+    if (focus) {
+        // Adjust the transparent percentage to make the radial background smaller
+        const transparentPercentage = 25; // Change this value as needed
+        focus.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(190, 255, 255, 0.1), transparent ${transparentPercentage}%)`;
+      }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return null;
+};
+
+export default FocusComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,15 @@ import ACT from './assets/act.png';
 import Zod from './assets/zod.png';
 import Duracoat from './assets/duracoat.png';
 import Link from '@/node_modules/next/link';
+import FocusComponent from './components/FocusComponent';
 
 export default function Home() {
 
 
   return (
     <main>
+      <div className="focus"></div>
+      <FocusComponent />
       <div className="container">
         <div className="left-block block">
           <h1 className='name'>Pratik Keluskar</h1>
@@ -65,7 +68,7 @@ Executing WordPress projects involving custom themes and premium themes, incorpo
       </div>
       <div className="projects-box">
         <Link href="/Projects">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m656-120-56-56 63-64-63-63 56-57 64 64 63-64 57 57-64 63 64 64-57 56-63-63-64 63Zm-416-80q17 0 28.5-11.5T280-240q0-17-11.5-28.5T240-280q-17 0-28.5 11.5T200-240q0 17 11.5 28.5T240-200Zm0 80q-50 0-85-35t-35-85q0-50 35-85t85-35q37 0 67.5 20.5T352-284q39-11 63.5-43t24.5-73v-160q0-83 58.5-141.5T640-760h46l-63-63 57-57 160 160-160 160-57-56 63-64h-46q-50 0-85 35t-35 85v160q0 73-47 128.5T354-203q-12 37-43.5 60T240-120Zm-64-480-56-56 63-64-63-63 56-57 64 64 63-64 57 57-64 63 64 64-57 56-63-63-64 63Z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"><path d="m656-120-56-56 63-64-63-63 56-57 64 64 63-64 57 57-64 63 64 64-57 56-63-63-64 63Zm-416-80q17 0 28.5-11.5T280-240q0-17-11.5-28.5T240-280q-17 0-28.5 11.5T200-240q0 17 11.5 28.5T240-200Zm0 80q-50 0-85-35t-35-85q0-50 35-85t85-35q37 0 67.5 20.5T352-284q39-11 63.5-43t24.5-73v-160q0-83 58.5-141.5T640-760h46l-63-63 57-57 160 160-160 160-57-56 63-64h-46q-50 0-85 35t-35 85v160q0 73-47 128.5T354-203q-12 37-43.5 60T240-120Zm-64-480-56-56 63-64-63-63 56-57 64 64 63-64 57 57-64 63 64 64-57 56-63-63-64 63Z"/></svg>
         </Link>
         <span className='hover'>Checkout Projects</span>
       </div>
